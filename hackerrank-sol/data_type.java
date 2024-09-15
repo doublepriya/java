d182fe7ff5eb2e278e92d50bc0c588f02ff23767
PRIYA SHARMA <143647952+doublepriya@users.noreply.github.com>
2024-09-15 11:12:58 +0530

    Create data_type.java

---
+++ hackerrank-sol/data_type.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            try {
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+
+                // Check for byte
+                if (x >= -128 && x <= 127) {
+                    System.out.println("* byte");
+                }
+
+                // Check for short
+                if (x >= -32768 && x <= 32767) {
+                    System.out.println("* short");
+                }
+
+                // Check for int
+                if (x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE) {
+                    System.out.println("* int");
+                }
+
+                // Check for long (no need for additional check since long is the largest)
+                System.out.println("* long");
+
+            } catch (Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
+        }
+
+        sc.close();
+    }
+}
